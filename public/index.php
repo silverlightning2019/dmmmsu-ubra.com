0b9cda8cd4d03a3dd4bec3cb20d454418a3b8a87
Silver Roi Valdez Ramos <silverroiramos13@gmail.com>
2022-01-27 12:46:11 +0800

    img directory

--- public/index.php
+++ public/index.php
@@ -146,7 +146,7 @@
 <section class="slider">
   <ul class="slides">
     <li>
-      <img src="image.jpg">
+      <img src="img/image.jpg">
       <div class="caption center-align">
         <h3 class="welcome">Welcome to <span class="logo">UBRA</span> : DMMMSU-MLUC Job Finder System</h3>
         <h5 class="slogan">"You can find the Job that suits you!"</h5><br>        
@@ -316,7 +316,7 @@
           <div class="col s12 m4">
             <div class="card hoverable">
               <div class="card-image">
-                <img src="Engineers.jpg">
+                <img src="img/Engineers.jpg">
               </div>
               <div class="card-content">
                 <p class="job">Civil Engineer</p>
@@ -331,7 +331,7 @@
          <div class="col s12 m4">
             <div class="card hoverable">
               <div class="card-image">
-                <img src="Engineers.jpg">
+                <img src="img/Engineers.jpg">
               </div>
               <div class="card-content">
                 <p class="job">Civil Engineer</p>
@@ -346,7 +346,7 @@
           <div class="col s12 m4">
             <div class="card hoverable">
               <div class="card-image">
-                <img src="Engineers.jpg">
+                <img src="img/Engineers.jpg">
               </div>
               <div class="card-content">
                 <p class="job">Civil Engineer</p>
@@ -430,7 +430,7 @@
           <div class="col s12 m3">
             <div class="card hoverable">
               <div class="card-image">
-                <img src="dmmmsu.jpg">
+                <img src="img/dmmmsu.jpg">
               </div>
               <div class="card-content">
                 <p class="center">Don Mariano Marcos Memorial State University</p><br>
