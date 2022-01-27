4a569938deea67224e542d5115cbf14544a70afe
Silver Roi Valdez Ramos <silverroiramos13@gmail.com>
2022-01-27 13:36:46 +0800

    link changes

--- public/index.php
+++ public/index.php
@@ -133,8 +133,8 @@
         <li><a href="GuestJobOffer.php">Job Offers</a></li>
         <li><a href="GuestJobEvents.php">Job Events</a></li>
         <li><a href="">My Applications</a></li>
-        <li><a href="">Log In</a></li>
-        <li><a href="">Sign Up</a></li>
+        <li><a href="login.php">Log In</a></li>
+        <li><a href="userregistration1">Sign Up</a></li>
         <li><a href="" class="waves-effect blue btn">Employer</a></li>
       </ul>
       <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons icon">menu</i></a>
