936eacdbeaaa01895d580697cf3dccdbe007864b
Silver Roi Valdez Ramos <silverroiramos13@gmail.com>
2022-01-27 12:39:45 +0800

    directory changes

--- public/index.php
+++ public/index.php
@@ -124,7 +124,7 @@
         <li><a href="GuestJobEvents.php">Job Events</a></li>
         <li><a href="">My Applications</a></li>
         <li><a href="login.php" class="waves-effect blue btn"><i class="material-icons left">login</i>Log In</a></li>
-        <li><a href="UserRegistration1.php" class="waves-effect blue btn"><i class="material-icons left">how_to_reg</i>Register</a></li>
+        <li><a href="userregistration1.php" class="waves-effect blue btn"><i class="material-icons left">how_to_reg</i>Register</a></li>
         <li><a href="UserRegistration1.php" class="waves-effect blue btn"><i class="material-icons left">business</i>Employer</a></li>
       </ul>
 
